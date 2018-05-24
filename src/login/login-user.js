1021ed68caf2667b9c8475da383eaabdb6e509b6
Dave Elliott <delliott@findmypast.com>
2018-05-24 17:26:43 +0100

    added a todo comment.

--- src/login/login-user.js
+++ src/login/login-user.js
@@ -3,5 +3,6 @@ const r = ReactRethinkdb.r;
 const superagent = require('superagent');
 
 export default function loginUser(email, password) {
+  // TODO: Add the username and password to this post and things should work !
   superagent.post('localhost:8015/login');
 }
